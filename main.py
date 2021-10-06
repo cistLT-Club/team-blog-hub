fddb1d586ad58e9dde9b6012edd974901055bef7
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-10-06 20:30:38 +0900

    デプロイを自動化

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import re
 def main(): 
     dt_now = datetime.datetime.now()
 
-    fr = open('README.md', 'r', encoding='UTF-8')
+    fr = open('README.md')
     contents = fr.read()
-    date = re.search(r'\<update-at>.+?\</update-at>', contents)
-    fix_date = dt_now.strftime('WEBサイトの最終更新 : ' + '%Y年%m月%d日 %H:%M:%S')
+    print(contents)
+    date = re.search(r'\<update-at>.+?\</update-at>', contents).group()
+    print(date)
+    fix_date = dt_now.strftime('<update-at>WEBサイトの最終更新 : ' + '%Y年%m月%d日 %H:%M:%S' + '</update-at>')
     fix_contents = contents.replace(str(date), str(fix_date))
     fw = open('README.md', 'w', encoding='UTF-8')
     fw.write(fix_contents)
